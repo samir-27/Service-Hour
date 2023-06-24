ac7c399be114831351b29e6cf336c9640ac82d98
samir-27 <sumrasamir27@gmail.com>
2023-06-24 09:05:55 +0530

    create navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="home">
+        <main>
+            <h1>Service Hour</h1>
+            <p>Solution to all Your problems</p>
+        </main>
+    </div>
+  )
+}
+
+export default Home
