25af3fe351f27c11766c852a8fd840ece055d324
samir-27 <sumrasamir27@gmail.com>
2023-06-25 10:55:34 +0530

    createing home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
-
+import vg from "../assets/2.webp"
 const Home = () => {
   return (
+   <>
     <div className="home">
         <main>
             <h1>Service Hour</h1>
             <p>Solution to all Your problems</p>
         </main>
     </div>
+    <div className="home2">
+      <img src={vg} alt="" />
+      <div>
+        <p>
+        At Solution Agency, we thrive on tackling complex problems head-on. With our diverse team of experts and innovative approach, we provide tailored solutions that propel businesses forward. From strategic planning to technological advancements, our comprehensive services empower organizations to overcome obstacles, achieve sustainable growth, and seize opportunities in a rapidly evolving world.
+        </p>
+      </div>
+
+    </div>
+    <div className="home3">
+      <div>
+        <h1>Who Are You</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error aperiam accusamus assumenda sunt,
+          
+           possimus voluptate magni reiciendis aut voluptatem exercitationem debitis.
+           Modi debitis porro nostrum facilis, unde doloremque corrupti impedit.
+           
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nulla error eum, suscipit facere quas quo saepe impedit facilis ad iusto voluptatibus vero! Commodi cumque dolor, veniam aliquid, voluptatibus, tempore sit illo optio aspernatur at ipsa doloremque vero. Qui quo magnam deleniti non doloribus officiis! Ipsam molestias repudiandae eos impedit!
+           
+           
+           </p>
+      </div>
+    </div>
+   </>
   )
 }
 
