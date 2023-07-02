97ba86638afc3de818a5c759e5b7fca3f69b361d
samir-27 <sumrasamir27@gmail.com>
2023-07-02 10:23:46 +0530

    add Services section

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import Services from "./components/Services";
 import "./styles/App.scss"
 import "./styles/Header.scss"
 import "./styles/Home.scss"
 import "./styles/Footer.scss"
 import "./styles/Contact.scss"
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/Services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
